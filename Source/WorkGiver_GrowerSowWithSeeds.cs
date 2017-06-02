dcdafc13e3dc956207d5b1928d029902d7720cd6
notfood <notfood@users.noreply.github.com>
2017-06-01 20:06:14 -0500

    Clear area before sowing

--- Source/WorkGiver_GrowerSowWithSeeds.cs
+++ Source/WorkGiver_GrowerSowWithSeeds.cs
@@ -11,8 +11,25 @@ namespace SeedsPlease
 	{
 		public override Job JobOnCell (Pawn pawn, IntVec3 c)
 		{
-			Job job = base.JobOnCell (pawn, c);
+			var job = base.JobOnCell (pawn, c);
+
+			// plant has seeds, if there is a seed return a job, otherwise prevent it.
 			if (job != null && job.plantDefToSow != null && job.plantDefToSow.blueprintDef != null) {
+
+				// Don't start until the zone is clear from blockers we can cut/move
+				var zone = c.GetZone (pawn.Map);
+				if (zone != null) {
+					foreach (var thing in zone.AllContainedThings) {
+						if (thing.def != job.plantDefToSow && thing.def.BlockPlanting && pawn.CanReserve (thing) && !thing.IsForbidden (pawn)) {
+							if (thing.def.category == ThingCategory.Plant) {
+								return new Job (JobDefOf.CutPlant, thing);
+							} else if (thing.def.EverHaulable) {
+								return HaulAIUtility.HaulAsideJobFor (pawn, thing);
+							}
+						}
+					}
+				}
+
 				Predicate<Thing> predicate = (Thing tempThing) =>
 					!ForbidUtility.IsForbidden (tempThing, pawn.Faction)
 					&& PawnLocalAwareness.AnimalAwareOf (pawn, tempThing)
